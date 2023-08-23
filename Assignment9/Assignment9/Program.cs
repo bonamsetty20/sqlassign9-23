99ca91b1cbba1df37da66a275a07a52c10050329
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-08-23 15:51:22 +0530

    first commit

---
+++ Assignment9/Assignment9/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment9
+{
+    internal class Program
+    {
+        static SqlConnection con;
+        static SqlCommand cmd;
+        static string conString = "server = DESKTOP-QHQ6E5M;database = OrderDB;trusted_connection = true;";
+      
+        static void Main(string[] args)
+        {
+            try
+            {
+                con = new SqlConnection(conString);
+                con.Open();
+
+                cmd = new SqlCommand()
+                {
+                    CommandText = "PlaceOrder",
+                    CommandType = CommandType.StoredProcedure,
+                    Connection = con
+                };
+
+                Console.Write("Enter Customer ID : ");
+                cmd.Parameters.AddWithValue("@customerId", int.Parse(Console.ReadLine()));
+                Console.Write("Enter Total Amount : ");
+                cmd.Parameters.AddWithValue("@totalAmount", double.Parse(Console.ReadLine()));
+
+                int output = (int)cmd.ExecuteScalar();
+
+                if (output >= 1)
+                {
+                    Console.WriteLine("Order Conformed!!!!");
+                }
+                else
+                {
+                    Console.WriteLine("Order Failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
